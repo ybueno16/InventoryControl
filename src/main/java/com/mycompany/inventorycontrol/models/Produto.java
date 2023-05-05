97346b19960d63d6743b3c0d21fa788f8e8e64ac
ybueno16 <yuribueno27@gmail.com>
2023-05-04 21:15:13 -0300

    Alteração Estruturação Pastas

--- src/main/java/com/mycompany/inventorycontrol/models/Produto.java
+++ src/main/java/com/mycompany/inventorycontrol/models/Produto.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.inventorycontrol.controllers;
+package com.mycompany.inventorycontrol.models;
 
 /**
  *
  * @author yuri
  */
-public class LoginController {
+public class Produto {
     
 }
