280583a62f5875cfcadedebf70b8df1aab900af6
ybueno16 <yuribueno27@gmail.com>
2023-05-18 21:38:41 -0300

    Implementação Metodo showProducts
    Foi implementado o metódo showProducts que mostra na tela todos os produtos cadastrados

--- src/main/java/com/mycompany/inventorycontrol/models/ProdutoShow.java
+++ src/main/java/com/mycompany/inventorycontrol/models/ProdutoShow.java
@@ -1,4 +1,32 @@
 package com.mycompany.inventorycontrol.models;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.SQLException;
+
 public class ProdutoShow {
+    public static class ShowProducts {
+        public static void showProducts(ProdutoShow Pshow) {
+            try {
+                Connection conexao = Produto.conexaoDAO.getInstancia().getConexao();
+                Statement statement = conexao.createStatement();
+                String query = "SELECT * FROM produto";
+                ResultSet resultSet = statement.executeQuery(query);
+                while (resultSet.next()) {
+                    int id = resultSet.getInt("id");
+                    String nome = resultSet.getString("nome");
+                    String descricao = resultSet.getString("descricao");
+                    double preco = resultSet.getDouble("preco");
+                    int quantidadeEstoque = resultSet.getInt("quantidade_estoque");
+                }
+                resultSet.close();
+                statement.close();
+                conexao.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+
+            }
+        }
+    }
 }
