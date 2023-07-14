5e3ca397247a87a0534d20080e3d02f95b0966d3
ybueno16 <yuribueno27@gmail.com>
2023-07-14 15:28:02 -0300

    Implemtação Edição Produto
    Implementação querys no banco

--- src/main/java/com/mycompany/inventorycontrol/controllers/ProdutoShowController.java
+++ src/main/java/com/mycompany/inventorycontrol/controllers/ProdutoShowController.java
@@ -3,15 +3,16 @@ package com.mycompany.inventorycontrol.controllers;
 import static com.mycompany.inventorycontrol.models.ProdutoShow.getProdutos;
 
 import com.mycompany.inventorycontrol.models.ProdutoShow;
-import com.mycompany.inventorycontrol.views.ProdutoShowView;
 import com.mycompany.inventorycontrol.views.ProdutoAddView;
+import com.mycompany.inventorycontrol.views.ProdutoEditView;
+import com.mycompany.inventorycontrol.views.ProdutoShowView;
+import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.event.MouseEvent;
 import java.awt.event.MouseAdapter;
-import java.awt.Point;
+import java.awt.event.MouseEvent;
 import java.util.List;
 
 public class ProdutoShowController {
@@ -22,60 +23,69 @@ public class ProdutoShowController {
     this.ProdutoShowView = produtoShowView;
 
     produtoShowView
-        .getAddProdutoButton()
-        .addActionListener(
-            new ActionListener() {
-              @Override
-              public void actionPerformed(ActionEvent e) {
-                exibirTelaAdicionarProduto();
-              }
-            });
-
-    produtoShowView
-        .getSearchFieldButton()
-        .addActionListener(
-            new ActionListener() {
-              @Override
-              public void actionPerformed(ActionEvent e) {
-                pesquisaProduto();
-              }
-            });
-
-    produtoShowView
-        .getUserConfig()
-        .addActionListener(
-            new ActionListener() {
-              @Override
-              public void actionPerformed(ActionEvent event) {
-                System.out.println("Menu clicado"); // Instanciar tela usuario
-              }
-            });
-
-    this.ProdutoShowView.getSearchField()
-        .addKeyListener(
-            new KeyAdapter() {
-              public void keyPressed(KeyEvent e) {
-                int keyCode = e.getKeyCode();
-                if (keyCode == KeyEvent.VK_ENTER) {
-                  System.out.println("Tecla pressionada");
-                  pesquisaProduto();
-                } else {
-                  // Nada acontece se outra tecla for pressionada
-                }
-              }
-            });
-
-    this.ProdutoShowView.getTable().addMouseListener(new MouseAdapter() {
-      public void mouseClicked(MouseEvent e) {
-        if (e.getClickCount() == 2) {
-          Point pnt = e.getPoint();
-          int row = produtoShowView.getTable().rowAtPoint(pnt);
-          if (row != -1) {
-            System.out.println("Abrindo tela de edição");
+      .getAddProdutoButton()
+      .addActionListener(
+        new ActionListener() {
+          @Override
+          public void actionPerformed(ActionEvent e) {
+            exibirTelaAdicionarProduto();
           }
         }
-      }
-    });
+      );
+
+    produtoShowView
+      .getSearchFieldButton()
+      .addActionListener(
+        new ActionListener() {
+          @Override
+          public void actionPerformed(ActionEvent e) {
+            pesquisaProduto();
+          }
+        }
+      );
+
+    produtoShowView
+      .getUserConfig()
+      .addActionListener(
+        new ActionListener() {
+          @Override
+          public void actionPerformed(ActionEvent event) {
+            System.out.println("Menu clicado"); // Instanciar tela usuario
+          }
+        }
+      );
+
+    this.ProdutoShowView.getSearchField()
+      .addKeyListener(
+        new KeyAdapter() {
+          public void keyPressed(KeyEvent e) {
+            int keyCode = e.getKeyCode();
+            if (keyCode == KeyEvent.VK_ENTER) {
+              System.out.println("Tecla pressionada");
+              pesquisaProduto();
+            } else {
+              // Nada acontece se outra tecla for pressionada
+            }
+          }
+        }
+      );
+
+    this.ProdutoShowView.getTable()
+      .addMouseListener(
+        new MouseAdapter() {
+          public void mouseClicked(MouseEvent e) {
+            if (e.getClickCount() == 2) {
+              Point pnt = e.getPoint();
+              int row = produtoShowView.getTable().rowAtPoint(pnt);
+              if (row != -1) {
+                int id = getIdFromTable();
+                System.out.println(id);
+                editarProduto(); //Instacia tela de edição de produto
+              }
+            }
+          }
+        }
+      );
   }
 
   public void carregarProdutos() {
@@ -92,11 +102,11 @@ public class ProdutoShowController {
 
   public void adicionarProdutoTabela(ProdutoShow produto) {
     Object[] row = new Object[] {
-        produto.getId(),
-        produto.getNome(),
-        produto.getDescricao(),
-        produto.getPreco(),
-        produto.getqntEstoque(),
+      produto.getId(),
+      produto.getNome(),
+      produto.getDescricao(),
+      produto.getPreco(),
+      produto.getqntEstoque(),
     };
     ProdutoShowView.getTableModel().addRow(row);
   }
@@ -114,5 +124,25 @@ public class ProdutoShowController {
     for (ProdutoShow produto : produtos) {
       adicionarProdutoTabela(produto);
     }
+  }
+
+  public void editarProduto() {
+    ProdutoEditView view = new ProdutoEditView();
+    ProdutoEditController controller = new ProdutoEditController();
+  }
+
+  public int getIdFromTable() {
+    int selectedRow = ProdutoShowView.getTable().getSelectedRow();
+    int selectedRowInModel = ProdutoShowView
+      .getTable()
+      .convertRowIndexToModel(selectedRow);
+    int idColumnIndex = ProdutoShowView
+      .getTable()
+      .getColumnModel()
+      .getColumnIndex("ID");
+    return (int) ProdutoShowView
+      .getTable()
+      .getModel()
+      .getValueAt(selectedRowInModel, idColumnIndex);
   }
 }
--- src/main/java/com/mycompany/inventorycontrol/models/ProdutoEdit.java
+++ src/main/java/com/mycompany/inventorycontrol/models/ProdutoEdit.java
@@ -1,20 +1,58 @@
 package com.mycompany.inventorycontrol.models;
 
 import com.mycompany.inventorycontrol.models.ProdutoAdd.conexaoDAO;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 
 public class ProdutoEdit {
-  private int id;
+
   private final String nome;
   private final String descricao;
   private final double preco;
   private final int qntEstoque;
   private static conexaoDAO instancia;
+  private int id;
 
   //Constructor
-  public ProdutoEdit(String nome, String descricao, double preco, int qntEstoque) {
+  public ProdutoEdit(
+    String nome,
+    String descricao,
+    double preco,
+    int qntEstoque
+  ) {
     this.nome = nome;
     this.descricao = descricao;
     this.preco = preco;
     this.qntEstoque = qntEstoque;
   }
+
+  public int getId() {
+    return this.id;
+  }
+
+
+
+  public class EditarProduto {
+
+    public boolean editarProduto(EditarProduto editarProduto) {
+      try {
+        Connection conexao = ProdutoAdd.conexaoDAO.getInstancia().getConexao();
+        String query =
+          "UPDATE produto SET nome = ?, descricao = ?, preco = ?, qntEstoque = ? WHERE id = ?";
+        PreparedStatement statement = conexao.prepareStatement(query);
+        statement.setString(1, nome);
+        statement.setString(2, descricao);
+        statement.setDouble(3, preco);
+        statement.setInt(4, qntEstoque);
+        statement.setInt(5, getId());
+        int rowsAffected = statement.executeUpdate();
+        conexao.close();
+      } catch (Exception e) {
+        // TODO: handle exception
+      }
+      return false;
+    }
+  }
+
 }
